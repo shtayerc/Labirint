d594832822b6bd46b9002775cd9bea6cf8fad455
shtayerc <david.murko5@gmail.com>
2017-01-14 20:01:09 +0100

    Testing autosave progress

--- include.js
+++ include.js
@@ -7,6 +7,25 @@ function include(url)
     head.appendChild(script);
     console.log('Included: '+url);
 }
+window.onbeforeunload = function() {
+    if(typeof progress!= 'undefined')
+    {
+        //simulacija post metode pri formi http://www.openjs.com/articles/ajax_xmlhttp_using_post.php
+        var http=new XMLHttpRequest();
+        var url="http://www2.scptuj.si/~murko.david1/Labirint/index.php";
+        var param="newprogress="+map.levelIndex;
+        http.open("POST",url,true);
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        http.setRequestHeader("Content-length", params.length);
+        http.setRequestHeader("Connection", "close");
+
+        http.send(param);
+    }
+    console.log(map.levelIndex);
+
+    return false;
+}     
+
 var path="./";
 include(path+'canvas.js');
 include(path+'main.js');
